1250f8b0e2091b0a0b2d1c20d4ad81fcc6a5e407
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-03 11:12:20 +0530

    connection

--- db.js
+++ db.js
@@ -1,2 +1,4 @@
 const mongoose = require('mongoose')
 
+
+mongodb+srv://kumarsaurabh2910:ygN11p9rXG9wu6EP@testprodb.cf1j1nf.mongodb.net/?retryWrites=true&w=majority
--- server.js
+++ server.js
@@ -105,46 +105,46 @@ console.log("notes server is avalible");
 
 /* create server with express */
 
-const express = require('express')
-const app = express()
+// const express = require('express')
+// const app = express()
 
-app.get('/', function (req, res) {
-  res.send('welcome to my how can i help you! ')           // menu of hotel 
-})
+// app.get('/', function (req, res) {
+//   res.send('welcome to my how can i help you! ')           // menu of hotel 
+// })
 
-app.get('/chicken', function (req,res)  {
-    var samosa ={
-        name: "samosa",
-        size: 10,
-        price : 20,
-        hotelname:"sahusahebhotelandcompany"
-    }
-    res.send(samosa)
-})
+// app.get('/chicken', function (req,res)  {
+//     var samosa ={
+//         name: "samosa",
+//         size: 10,
+//         price : 20,
+//         hotelname:"sahusahebhotelandcompany"
+//     }
+//     res.send(samosa)
+// })
 
 
 
-app.get('/saurabh', function (req,res){
+// app.get('/saurabh', function (req,res){
 
-    var details ={
-        name:"Saurabh Kumar",
-        email:"saurabhkumarsingh@gmail.com",
-        age:24,
-        address:"A-79 sec-33 Noida ",
-        hobbies:"Reading novels,books,coding"
-    }
-    res.send(details)
+//     var details ={
+//         name:"Saurabh Kumar",
+//         email:"saurabhkumarsingh@gmail.com",
+//         age:24,
+//         address:"A-79 sec-33 Noida ",
+//         hobbies:"Reading novels,books,coding"
+//     }
+//     res.send(details)
 
-})
+// })
 
-app.post('/details', function (req,res){
+// app.post('/details', function (req,res){
 
-    res.send("details data has been saved")
-})
+//     res.send("details data has been saved")
+// })
 
-app.listen(3000, ()=>{
-    console.log("server is listing into the hotel");
-})   //address of waiter
+// app.listen(3000, ()=>{
+//     console.log("server is listing into the hotel");
+// })   //address of waiter
 
 
 
