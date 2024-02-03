817554bbdcea64becdd6e211abb56090bbe69901
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-03 11:46:00 +0530

    db.js

--- db.js
+++ db.js
@@ -7,3 +7,23 @@ mongoose.connect("mongodb+srv://kumarsaurabh2910:ygN11p9rXG9wu6EP@testprodb.cf1j
 })
 
 const db = mongoose.connection
+
+
+db.on('connected', ()=>{
+
+    console.log('connected to Mongodb Server');
+})
+
+db.on('error', (err)=>{
+
+    console.log('Mongodb connection error', err);
+})
+
+
+db.on('disconnected', ()=>{
+
+    console.log('Mongodb disconnected');
+})
+// Exports the database connnection
+module.exports = db
+
