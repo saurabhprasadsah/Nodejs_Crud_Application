cf5d103d4c318803156f2cbae3a97c7a7487a21d
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2024-02-11 20:27:29 +0530

    commit

--- server.js
+++ server.js
@@ -1,30 +1,31 @@
- const express = require('express')
- const app = express()
+const express = require('express')
+const app = express()
 
 
- app.get('/', function (req, res) {
-   res.send('welcome to my how can i help you! ')          
- })
+app.get('/', function (req, res) {
+    res.send('welcome to my how can i help you! ')
+})
 
- app.get('/saurabh', function (req,res){
-     var details ={
-         name:"Saurabh Kumar",
-         email:"saurabhkumarsingh@gmail.com",
-         age:24,
-         address:"A-79 sec-33 Noida ",
-         hobbies:"Reading novels,books,coding"
-     }
-     res.send(details)
+app.get('/saurabh', function (req, res) {
+    var details = {
+        name: "Saurabh Kumar",
+        email: "saurabhkumarsingh@gmail.com",
+        age: 24,
+        address: "A-79 sec-33 Noida ",
+        hobbies: "Reading novels,books,coding"
+    }
+    res.send(details)
 
- })
+})
 
- app.post('/details', function (req,res){
-      res.send("details data has been saved")
-  })
+app.post('/details', function (req, res) {
+    res.send("details data has been saved")
+})
 
-  app.listen(3000, ()=>{
-      console.log("server is listing into the hotel");
-  })   
+
+app.listen(3000, () => {
+    console.log("server is listing into the hotel");
+})
 
 
 
